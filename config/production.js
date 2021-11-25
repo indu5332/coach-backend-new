3fd6b50c6404c263b4946aabb2fa1d8a6e25dae8
indu5332 <nairagarg999@gmail.com>
2021-11-25 11:06:37 +0530

    first commit

--- config/production.js
+++ config/production.js
@@ -1,9 +1,9 @@
 module.exports = {
-  "hosts": ["https://just-order-api.herokuapp.com/","*"],
-  allowedOrigins: ["https://just-order-api.herokuapp.com/","*"],
+  "hosts": ["https://coach-backend-new.herokuapp.com/","*"],
+  allowedOrigins: ["https://coach-backend-new.herokuapp.com/","*"],
   DB_HOST:process.env.DB_HOST,
   "VERSION":"v1",
   secret: process.env.secret,
   imagePath: path.join(__dirname, "../uploads/"),
-  fileUrl: "https://just-order-api.herokuapp.com/",
+  fileUrl: "https://coach-backend-new.herokuapp.com/",
 };
