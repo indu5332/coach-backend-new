80f0db860b3683ca8c16185995ca864edb1d5676
indu5332 <nairagarg999@gmail.com>
2021-11-25 10:48:20 +0530

    first commit

--- config/production.js
+++ config/production.js
@@ -1,3 +1,7 @@
 module.exports = {
   DB_HOST: process.env.DB_HOST,
+  secret: process.env.secret,
+  allowedOrigins: ["https://coach-backend-new.herokuapp.com/"],
+  imagePath: path.join(__dirname, "../uploads/"),
+  fileUrl: "https://coach-backend-new.herokuapp.com/",
 };
