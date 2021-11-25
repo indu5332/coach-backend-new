1f9e35a632380b54810c48b867b6cac0e155c67e
indu5332 <nairagarg999@gmail.com>
2021-11-25 10:58:35 +0530

    first commit

--- config/production.js
+++ config/production.js
@@ -1,7 +1,7 @@
 module.exports = {
   "hosts": ["https://just-order-api.herokuapp.com/","*"],
   allowedOrigins: ["https://just-order-api.herokuapp.com/","*"],
-  DB_HOST:"https://just-order-api.herokuapp.com/",
+  DB_HOST:DB_HOST:process.env.DB_HOST,
   "VERSION":"v1",
   secret: process.env.secret,
   imagePath: path.join(__dirname, "../uploads/"),
