09087eea392f66b10c5039576316b344ef10a63f
indu5332 <nairagarg999@gmail.com>
2021-12-30 09:20:59 +0530

    add duration

--- config/production.js
+++ config/production.js
@@ -5,7 +5,7 @@ module.exports = {
   HOST:"https://coach-backend-new.herokuapp.com",
   host:"https://coach-champion-admin.web.app",
   hosts: [
-    "https://coach-backend-new.herokuapp.com","http://localhost:3000","http://localhost:3002","https://coachfabienchampion.web.app"
+    "https://coach-backend-new.herokuapp.com","http://localhost:3000","http://localhost:3001","https://coachfabienchampion.web.app"
   ],
   secret: process.env.secret,
   tokenDuration: "30d",
